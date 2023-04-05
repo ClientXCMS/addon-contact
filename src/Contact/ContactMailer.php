5ebe8fe68edc611c6271bc4217cbf7308d05f879
Martindev <Delebecque.martin@gmail.com>
2023-04-06 01:39:08 +0200

    Update ContactMailer.php

--- src/Contact/ContactMailer.php
+++ src/Contact/ContactMailer.php
@@ -7,6 +7,7 @@ use ClientX\Database\EmailMessageTable;
 use ClientX\Notifications\Mailer\MailMessage;
 use ClientX\Notifications\Mailer\Support\SwiftMailer;
 use ClientX\Router;
+use ClientX\Services\LocaleService;
 
 class ContactMailer
 {
@@ -14,17 +15,21 @@ class ContactMailer
     private SwiftMailer $mailer;
     private Router $router;
     private EmailMessageTable $table;
+    private LocaleService $localeService;
+    private string $to;
 
-    public function __construct(SwiftMailer $mailer, Router $router, string $to, EmailMessageTable $table)
+    public function __construct(SwiftMailer $mailer, Router $router, string $to, EmailMessageTable $table, LocaleService $localeService)
     {
-        $this->mailer   = $mailer;
-        $this->router   = $router;
-        $this->to       = empty($to) ? 'contact@clientxcms.com' : $to;
-        $this->table    = $table;
+        $this->mailer = $mailer;
+        $this->router = $router;
+        $this->to = empty($to) ? 'contact@clientxcms.com' : $to;
+        $this->table = $table;
+        $this->localeService = $localeService;
     }
+
     public function sendTo(Contact $contact)
     {
-        $template = $this->table->getConfiguredCustomTemplate('contact');
+        $template = $this->table->getConfiguredCustomTemplate('contact', $this->localeService->retrive());
         $link = $this->router->generateURIAbsolute('admin.contacts.edit', ['id' => $contact->getId()]);
         $message = new EmailMessage();
         $template->setContext([
