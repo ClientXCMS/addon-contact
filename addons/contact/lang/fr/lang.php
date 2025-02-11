04969b3d6df6d4e009605840957263729d525bb5
Cauctoflo <florentin.devs@gmail.com>
2025-02-11 14:12:28 +0100

    Language update

--- addons/contact/lang/fr/lang.php
+++ addons/contact/lang/fr/lang.php
@@ -36,5 +36,23 @@ return [
         'update_failed' => 'La mise à jour de la demande de contact a échoué'
     ],
     'confirm_delete' => 'Êtes-vous sûr de vouloir supprimer cette demande de contact ?',
-    'status_updated_at' => 'Statut mis à jour le :date'
+    'status_updated_at' => 'Statut mis à jour le :date',
+    'settings' => [
+        'title' => 'Paramètres',
+        'description' => 'Configurer les paramètres du module de contact',
+        'general_settings' => 'Paramètres généraux',
+        'security_settings' => 'Paramètres de sécurité',
+        'media_settings' => 'Paramètres médias',
+        'enable_captcha' => 'Activer le captcha',
+        'enable_captcha_description' => 'Ajouter une protection supplémentaire contre les spams',
+        'require_login' => 'Requérir une connexion',
+        'require_login_description' => 'Les utilisateurs doivent être connectés pour envoyer un message',
+        'page_image' => 'Image de la page',
+        'click_to_upload' => 'Cliquez pour télécharger',
+        'or_drag_and_drop' => 'ou glissez-déposez',
+        'up_to_2mb' => 'jusqu\'à 2 Mo',
+        'save_settings' => 'Sauvegarder les paramètres',
+        'disabled' => 'Désactivé',
+        'enabled' => 'Activé'
+    ]
 ];
