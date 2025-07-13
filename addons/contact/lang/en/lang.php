100a6655943ce8d92bab29157e42c2f27e396d48
Martin DELEBECQUE <delebecque.martin@gmail.com>
2025-07-13 12:59:05 +0200

    NEXTGEN import - contact (2025-07-13 12:59:05)

--- addons/contact/lang/en/lang.php
+++ addons/contact/lang/en/lang.php
@@ -1,42 +1,56 @@
 <?php
 
-/*
- * This file is part of the CLIENTXCMS project.
- * This file is the property of the CLIENTXCMS association. Any unauthorized use, reproduction, or download is prohibited.
- * For more information, please consult our support: clientxcms.com/client/support.
- * Year: 2024
- */
 return [
-    'title' => 'Contacts',
-    'management_title' => 'Contact Request Management',
-    'management_description' => 'List of all contact requests',
-    'show_title' => 'Contact Details',
-    'show_description' => 'View contact request details',
-    'table' => [
-        'id' => '#',
-        'title' => 'Subject',
-        'email' => 'Email',
-        'status' => 'Status',
-        'message' => 'Message',
-        'created_at' => 'Created at',
-        'actions' => 'Actions',
-    ],
-    'actions' => [
-        'view' => 'View',
-        'delete' => 'Delete',
-        'back' => 'Back',
-        'save' => 'Save',
-        'delete_success' => 'Contact request has been successfully deleted',
-        'update_success' => 'Contact request status has been successfully updated',
-    ],
-    'status' => [
-        'read' => 'Read',
-        'unread' => 'Unread',
-    ],
-    'errors' => [
-        'not_found' => 'Contact request not found',
-        'update_failed' => 'Contact request update failed',
-    ],
-    'confirm_delete' => 'Are you sure you want to delete this contact request?',
-    'status_updated_at' => 'Status updated at :date',
+  'title' => 'Contacts',
+  'pending' => 'Pending contact requests',
+  'description' => 'Handle your users\' contact requests in your client area.',
+  'management_title' => 'Management of contact requests',
+  'management_description' => 'List of all contact requests',
+  'show_title' => 'Contact details',
+  'show_description' => 'See the details of the contact request',
+  'title_page' => 'communicate with us',
+  'subject' => 'Subject of the request',
+  'content_label' => 'Describe your request',
+  'object_label' => 'Subject of the request',
+  'max_characters' => 'Limited to :max characters',
+  'send' => 'Send the request',
+  'subtitle_page' => 'We would be delighted to receive your messages. Please fill out the form below and we will respond to you as soon as possible.',
+  'actions' => [
+    'delete_success' => 'The contact request has been successfully deleted.',
+    'update_success' => 'The status of the contact request has been successfully updated.',
+    'reply_email' => 'Reply by email',
+    'send_mail' => 'Send an email to the client',
+    'limit_error' => 'You have already sent a contact request less than 5 minutes ago.',
+    'store_success' => 'The contact request has been successfully recorded.',
+  ],
+  'status' => [
+    'read' => 'document',
+    'unread' => 'Untreated',
+  ],
+  'webhook' => [
+    'title' => 'New contact request',
+    'description' => 'A new contact request has been received',
+  ],
+  'errors' => [
+    'not_found' => 'The contact request was not found',
+    'update_failed' => 'The update of the contact request failed.',
+  ],
+  'status_updated_at' => 'Status updated on :date',
+  'settings' => [
+    'title' => 'Contact page preferences',
+    'description' => 'Set your preferences for the contact page.',
+    'security_settings' => 'Security settings',
+    'media_settings' => 'Media settings',
+    'enable_captcha' => 'Enable the captcha',
+    'enable_captcha_description' => 'Add an extra layer of protection against spam',
+    'webhook' => 'Notify an external URL',
+    'webhook_description' => 'Send contact requests to an external URL',
+    'subscribers' => 'Notify the staff',
+    'subscribers_description' => 'Send contact requests to specific personnel',
+    'require_login' => 'Require a connection',
+    'require_login_description' => 'Users must be logged in to send a message.',
+    'page_image' => 'Image of the page',
+    'disabled' => 'inactive',
+    'enabled' => 'start',
+  ],
 ];
